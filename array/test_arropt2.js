573eddb3d13b394482ae0a930a21e316c2a7a874
jedichou <jedigit@163.com>
2020-10-02 21:30:13 +0800

    增加js数组深拷贝和浅拷贝

--- array/test_arropt2.js
+++ array/test_arropt2.js
@@ -11,5 +11,24 @@ describe('2.2.4 对数组的整体性操作', function(){
         done();
     });
 
-    
+    it('数组的浅拷贝', function(done){
+        var nums1 = [1,2,3];
+        var nums2 = nums1;
+        nums2[0] = 400;
+        assert.strictEqual(nums1[0], 400);
+        assert.strictEqual(nums2[0], 400);
+        done();
+    });
+
+    it('数组的深拷贝', function(done){
+        var nums1 = [1,2,3];
+        var nums2 = [];
+        for (var i=0; i<nums1.length; i++)
+            nums2[i] = nums1[i];
+        nums2[0] = 400;
+        assert.strictEqual(nums1[0],  1);
+        assert.strictEqual(nums2[0], 400);
+        done();
+    });
+
 });
