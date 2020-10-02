a82bc9d70904250d2bdc190c886dffee75c11012
jedichou <jedigit@163.com>
2020-10-02 20:11:15 +0800

    格式化代码

--- array/test_arropt.js
+++ array/test_arropt.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 
 describe('2.2.2　读写数组', function(){
+
     it('数组赋值', function(done){
         var arr = [];
         for (var i=0; i<5; ++i)
@@ -8,6 +9,7 @@ describe('2.2.2　读写数组', function(){
         assert.strictEqual(arr.length, 5);
         done();
     });
+
     it('读取数组元素', function(done){
         var arr = [1,2,3];
         assert.strictEqual(arr[0], 1);
@@ -15,12 +17,14 @@ describe('2.2.2　读写数组', function(){
         assert.strictEqual(arr[2], 3);
         done();
     });
+
     it('数组元素变更', function(done){
         var arr = [];
         arr[0] = 23;
         assert.strictEqual(arr[0], 23);
         done();
     });
+
     it('数组元素迭代1', function(done){
         var arr=[1,2];
         var sum = 0;
@@ -28,5 +32,6 @@ describe('2.2.2　读写数组', function(){
             sum += arr[i];
         assert.strictEqual(sum, 3);
         done();
-    });   
+    }); 
+      
 });
