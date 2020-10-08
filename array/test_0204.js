cc3b115b2b921667199741ed676cb7dc33ee7eb9
jedichou <jedigit@163.com>
2020-10-08 15:22:40 +0800

    第一个元素放到数组末尾

--- array/test_0204.js
+++ array/test_0204.js
@@ -86,6 +86,16 @@ describe('2.4 可变函数', function(){
             done();
         });
 
+        it('第一个元素放到数组末尾', function(done){
+            var arr = [3,1,2];
+            arr.push(arr.shift());
+            assert.strictEqual(arr.length, 3);
+            assert.strictEqual(arr[0], 1);
+            assert.strictEqual(arr[1], 2);
+            assert.strictEqual(arr[2], 3);
+            done();
+        });
+
     });
 
 });
