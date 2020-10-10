ec66672e05f1277c3e0367d32b925bd16bf9532b
jedichou <jedigit@163.com>
2020-10-10 11:43:46 +0800

    提交2.9(练习3(部分)代码

--- array/test_0209.js
+++ array/test_0209.js
@@ -49,4 +49,65 @@ describe('2.9 练习', function(){
 
     });
 
+    describe('2.9-练习3', function(){
+
+        // 3. 修改本章前面出现过的weeklyTemps 对象，使它可以使用一个二维数组
+        // 来存储每月的有用数据。增加一些方法用以显示月平均数、具体某一周平均数
+        // 和所有周的平均数。
+
+        function weekTemps() {
+            this.dataStore = [];
+        }
+
+        weekTemps.prototype.add = function (month, temp) {
+            if (!(this.dataStore[month - 1] instanceof Array))
+                this.dataStore[month - 1] = [];
+            this.dataStore[month - 1].push(temp);
+        };
+
+        weekTemps.prototype.avgmonth = function (month) {
+            var total = 0;
+            if (month < 1) return total;
+            if (!(this.dataStore[month-1] instanceof Array)) return total;
+            for (var idx = 0; idx < this.dataStore[month-1].length; idx++)
+                total += this.dataStore[month-1][idx];
+            return total / this.dataStore[month-1].length;
+        };
+        
+        it('2.9.2-weeklyTemp的测试-初始化', function(done){
+            var weeks = new weekTemps();
+            assert.strictEqual(weeks.dataStore.length, 0);
+            done();
+        });
+
+        it('2.9.2-weeklyTemp的测试-add-单个月份', function(done){
+            var weeks = new weekTemps();
+            weeks.add(1, 20);
+            weeks.add(1, 30);
+            assert.strictEqual(weeks.dataStore.length, 1);
+            assert.strictEqual(weeks.dataStore[0][0], 20);
+            assert.strictEqual(weeks.dataStore[0][1], 30);
+            done();
+        });
+
+        it('2.9.2-weeklyTemp的测试-add-多个月份', function(done){
+            var weeks = new weekTemps();
+            weeks.add(1, 20);
+            weeks.add(1, 30);
+            weeks.add(3, 40);
+            weeks.add(3, 50);
+            assert.strictEqual(weeks.dataStore.length, 3);
+            assert.strictEqual(weeks.dataStore[2][0], 40);
+            assert.strictEqual(weeks.dataStore[2][1], 50);
+            done();
+        });
+
+        it('2.9.2-weeklyTemp的测试-avgmonth-月份记录为空', function(done){
+            var weeks = new weekTemps();
+            assert.strictEqual(weeks.avgmonth(3), 0);
+            done();
+        });
+
+    });
+
 });
