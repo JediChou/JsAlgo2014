04c1d9b28a75bd3916eeaa1ba20621feac5515b1
jedichou <jedigit@163.com>
2020-10-10 21:48:17 +0800

    实现List的Append

--- list/list.js
+++ list/list.js
@@ -5,21 +5,32 @@ exports.List = function() {
     // Properties
     this.listSize = 0;
     this.pos = 0;
-    this.length = length;
+    // this.length = length;
     
     // Methods
-    this.clear = clear;
-    this.find = find;
-    this.toString = toString;
-    this.insert = insert;
-    this.append = append;
-    this.remove = remove;
-    this.front = front;
-    this.end = end;
-    this.prev = prev;
-    this.next = next;
-    this.currPos = currPos;
-    this.moveTo = moveTo;
-    this.getElement = getElement;
-    this.contains = contains;
+    
+    // append method
+    this.append = function(element) {
+ 
+        // 平易近人的写法
+        // this.dataStore.push(element);
+        // this.listSize++;
+
+        // 短码写法
+        this.dataStore[this.listSize++] = element;
+    };
+    
+    // this.clear = clear;
+    // this.find = find;
+    // this.toString = toString;
+    // this.insert = insert;
+    // this.remove = remove;
+    // this.front = front;
+    // this.end = end;
+    // this.prev = prev;
+    // this.next = next;
+    // this.currPos = currPos;
+    // this.moveTo = moveTo;
+    // this.getElement = getElement;
+    // this.contains = contains;
 };
