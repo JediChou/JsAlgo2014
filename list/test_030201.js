9e4313ebd4e5f96065316ce74bd34aab1c2b663e
jedichou <jedigit@163.com>
2020-10-10 22:12:26 +0800

    建立一个对象List

--- list/test_030201.js
+++ list/test_030201.js
@@ -24,4 +24,25 @@ describe('3.2.1 append：给列表添加元素', function(){
         done();
     });
 
+    it('建立一个字符串List', function(done){
+        var list = new collection.List();
+        list.append('jedi');
+        list.append('becky');
+        assert.strictEqual(list.dataStore.length, 2);
+        assert.strictEqual(list.listSize, 2);
+        assert.strictEqual(list.dataStore[0], 'jedi');
+        assert.strictEqual(list.dataStore[1], 'becky');
+        done();
+    });
+
+    it('建立一个对象List', function(done){
+        var list = new collection.List();
+        list.append({name:'jedi', age:42});
+        list.append({name:'becky', age:37});
+        assert.strictEqual(list.dataStore.length, 2);
+        assert.strictEqual(list.listSize, 2);
+        assert.deepStrictEqual(list.dataStore[0], {name:'jedi', age:42});
+        assert.deepStrictEqual(list.dataStore[1], {name:'becky', age:37});
+        done();
+    });
 });
