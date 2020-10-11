030f03f11a473094fc5bcb084b2cdca11d9c745c
jedichou <jedigit@163.com>
2020-10-11 15:37:46 +0800

    删除字符串列表中的某项

--- list/test_030202.js
+++ list/test_030202.js
@@ -21,4 +21,23 @@ describe('3.2.2 remove：从列表中删除元素', function(){
         done();
     });
 
+    it('删除字符串列表中的某项', function(done){
+        var lst = new collection.List();
+        lst.append("jedi");
+        lst.append("becky");
+        lst.append("cici");
+
+        var action = lst.remove("jedi");
+        assert.strictEqual(lst.listSize, 2);
+        assert.strictEqual(lst.dataStore.length, 2);
+        assert.strictEqual(lst.dataStore[0], "becky");
+        assert.strictEqual(lst.dataStore[1], "cici");
+
+        action = lst.remove("cici");
+        assert.strictEqual(lst.listSize, 1);
+        assert.strictEqual(lst.dataStore[0], "becky");
+        assert.ok(action);
+        done();
+    });
+
 });
