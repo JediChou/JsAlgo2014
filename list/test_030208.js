bcd9448b74a3154f50d5b000e6a36a9cc46d1774
jedichou <jedigit@163.com>
2020-10-12 17:15:46 +0800

    检测列表中是否包含boolean和number变量

--- list/test_030208.js
+++ list/test_030208.js
@@ -14,6 +14,25 @@ describe('3.2.8 contains：判断给定值是否在列表中', function(){
         done();
     });
 
+    it('检测列表中是否包含boolean变量', function(done){
+        var list = new collection.List();
+        list.append('test');
+        list.append(false);
+        list.append(true);
+        assert.ok(list.contains(false));
+        assert.ok(list.contains(true));
+        done();
+    });
+
+    it('检测列表中包含number变量', function(done){
+        var list = new collection.List();
+        list.append('test');
+        list.append(1);
+        list.append(Math.PI);
+        assert.ok(list.contains(1));
+        assert.ok(list.contains(Math.PI));
+        done();
+    });
     // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
     // JavaScript basic types
     // Data Types: undefined, Boolean, Number, String, BigInt, Symbol
