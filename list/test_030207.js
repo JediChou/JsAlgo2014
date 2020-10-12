204848292b53e8cf9ff90e58185a7faf87b8dfc9
jedichou <jedigit@163.com>
2020-10-12 15:25:57 +0800

    清空boolean列表

--- list/test_030207.js
+++ list/test_030207.js
@@ -6,12 +6,6 @@ var collection = require('./list.js');
 
 describe('3.2.7 clear：清空列表中所有的元素', function(){
 
-    // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
-    // JavaScript basic types
-    // Data Types: undefined, Boolean, Number, String, BigInt, Symbol
-    // Structural Types:
-    //   Object (Normal, Array, Map, Set, WeakMap, WeakSet, Date)
-    // Structrual Root Primitive: null
     it('清空undefined列表', function(done){
         var list = new collection.List();
         list.append(undefined);
@@ -23,5 +17,23 @@ describe('3.2.7 clear：清空列表中所有的元素', function(){
         done();
     });
 
+    it('清空boolean列表', function(done){
+        var list = new collection.List();
+        list.append(true);
+        list.append(false);
+        list.append(true);
+        list.clear();
+        assert.strictEqual(list.length(), 0);
+        assert.strictEqual(list.dataStore.length, 0);
+        assert.deepStrictEqual(list.dataStore, []);
+        done();
+    });
+
+    // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+    // JavaScript basic types
+    // Data Types: undefined, Boolean, Number, String, BigInt, Symbol
+    // Structural Types:
+    //   Object (Normal, Array, Map, Set, WeakMap, WeakSet, Date)
+    // Structrual Root Primitive: null
 });
 
