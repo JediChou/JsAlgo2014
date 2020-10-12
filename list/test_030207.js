44bf842b5fa069592beaafbd24946a5d8dfd65c2
jedichou <jedigit@163.com>
2020-10-12 16:43:02 +0800

    清空map列表

--- list/test_030207.js
+++ list/test_030207.js
@@ -99,6 +99,17 @@ describe('3.2.7 clear：清空列表中所有的元素', function(){
         assert.deepStrictEqual(list.dataStore, []);
         done();
     });
+
+    it('清空map列表', function(done){
+        var list = new collection.List();
+        var map = new Map();
+        appendRange(list, [map, map, map]);
+        list.clear();
+        assert.strictEqual(list.length(), 0);
+        assert.strictEqual(list.dataStore.length, 0);
+        assert.deepStrictEqual(list.dataStore, []);
+        done();
+    });
     // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
     // JavaScript basic types
     // Data Types: undefined, Boolean, Number, String, BigInt, Symbol
