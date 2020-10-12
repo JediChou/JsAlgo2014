4f2548588199436dbe77dd809f0aac24baae3e31
jedichou <jedigit@163.com>
2020-10-12 16:48:29 +0800

    清空null列表

--- list/test_030207.js
+++ list/test_030207.js
@@ -154,6 +154,16 @@ describe('3.2.7 clear：清空列表中所有的元素', function(){
         assert.deepStrictEqual(list.dataStore, []);
         done(); 
     });
+
+    it('清空null列表', function(done){
+        var list = new collection.List();
+        appendRange(list, [null, null, null]);
+        list.clear();
+        assert.strictEqual(list.length(), 0);
+        assert.strictEqual(list.dataStore.length, 0);
+        assert.deepStrictEqual(list.dataStore, []);
+        done(); 
+    });
     // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
     // JavaScript basic types
     // Data Types: undefined, Boolean, Number, String, BigInt, Symbol
