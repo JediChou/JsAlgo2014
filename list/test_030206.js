4df42be531b970450b715c00a116375d78666386
jedichou <jedigit@163.com>
2020-10-12 13:36:15 +0800

    向List中插入undefined值

--- list/test_030206.js
+++ list/test_030206.js
@@ -15,4 +15,22 @@ describe('3.2.6 insert：向列表中插入一个元素', function(){
         done();
     });
 
+    ///////////////////////////////////////////////////////////////////////////////
+    // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+    // JavaScript basic types
+    // Data Types: undefined, Boolean, Number, String, BigInt, Symbol
+    // Structural Types:
+    //   Object (Normal, Array, Map, Set, WeakMap, WeakSet, Date)
+    // Structrual Root Primitive: null
+
+    it('向List中插入undefined值', function(done){
+        var list = new collection.List();
+        list.append(undefined);
+        var action = list.insert(undefined, undefined);
+        assert.strictEqual(list.length(), 2);
+        assert.ok(action);
+        assert.strictEqual(list.dataStore[1], undefined);
+        done();
+    });
+
 });
