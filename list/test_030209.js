6c3815426def3c6b257f69f7994cc611a41eeb15
jedichou <jedigit@163.com>
2020-10-14 10:08:10 +0800

    front测试(undefined列表)

--- list/test_030209.js
+++ list/test_030209.js
@@ -19,7 +19,24 @@ describe('3.2.9 遍历列表', function(){
             assert.strictEqual(list.getElement(), 1);
             done();
         });
-        
+
+        it('front测试(undefined列表)', function(done){
+            var list = new collection.List();
+            list.append(undefined);
+            list.append(undefined);
+            list.append(undefined);
+            list.front();
+            assert.strictEqual(list.length(), 3);
+            assert.strictEqual(list.pos, 0);
+            assert.strictEqual(list.getElement(), undefined);
+            done();
+        });
+        // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+        // JavaScript basic types
+        // Data Types: undefined, Boolean, Number, String, BigInt, Symbol
+        // Structural Types:
+        //   Object (Normal, Array, Map, Set, WeakMap, WeakSet, Date)
+        // Structrual Root Primitive: null
     });
 
 });
