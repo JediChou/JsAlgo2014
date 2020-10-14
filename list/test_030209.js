40a93b9ed053d0b2c0ef1848ccfdad1d4e04df37
jedichou <jedigit@163.com>
2020-10-14 09:50:00 +0800

    扩展front测试

--- list/test_030209.js
+++ list/test_030209.js
@@ -6,15 +6,20 @@ var collection = require('./list.js');
 
 describe('3.2.9 遍历列表', function(){
 
-    it('front测试', function(done){
-        var list = new collection.List();
-        list.append(1);
-        list.append(2);
-        list.append(3);
-        list.front();
-        assert.strictEqual(list.length(), 3);
-        assert.strictEqual(list.pos, 0);
-        assert.strictEqual(list.getElement(), 1);
-        done();
+    describe('front测试', function(){
+        
+        it('front的normal测试', function(done){
+            var list = new collection.List();
+            list.append(1);
+            list.append(2);
+            list.append(3);
+            list.front();
+            assert.strictEqual(list.length(), 3);
+            assert.strictEqual(list.pos, 0);
+            assert.strictEqual(list.getElement(), 1);
+            done();
+        });
+        
     });
+
 });
