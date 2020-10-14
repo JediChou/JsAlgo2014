25f0124881436582b97de7d7253f393d77f6f29f
jedichou <jedigit@163.com>
2020-10-14 09:47:56 +0800

    front测试

---
+++ list/test_030209.js
@@ -0,0 +1,20 @@
+// file: test_030209.js
+// description: 测试List的相关迭代器方法.
+
+var assert = require('assert');
+var collection = require('./list.js');
+
+describe('3.2.9 遍历列表', function(){
+
+    it('front测试', function(done){
+        var list = new collection.List();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.front();
+        assert.strictEqual(list.length(), 3);
+        assert.strictEqual(list.pos, 0);
+        assert.strictEqual(list.getElement(), 1);
+        done();
+    });
+});
